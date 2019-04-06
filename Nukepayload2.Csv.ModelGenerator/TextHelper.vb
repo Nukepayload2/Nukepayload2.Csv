25ac810dd4e3720e83a689acbefea3e07502876c
Nukepayload2 <1939357182@qq.com>
2019-04-06 17:16:36 +0800

    Fixed NullRef in model generator

--- Nukepayload2.Csv.ModelGenerator/TextHelper.vb
+++ Nukepayload2.Csv.ModelGenerator/TextHelper.vb
@@ -3,7 +3,7 @@
 Module TextHelper
     <Extension>
     Function IsDateTime(txt As String) As Boolean
-        If txt.Length = 0 Then
+        If txt = Nothing Then
             Return False
         End If
         If Not Char.IsDigit(txt(0)) Then
@@ -13,16 +13,25 @@ Module TextHelper
     End Function
     <Extension>
     Function IsInteger(txt As String) As Boolean
+        If txt = Nothing Then
+            Return False
+        End If
         If txt.StartsWith("-") OrElse txt.StartsWith("+") Then txt = txt.Substring(1)
         If txt.Length = 0 Then Return False
         Return txt.Length = Aggregate c In From t In txt.ToCharArray Take While t >= "0"c AndAlso t <= "9"c Into Count
     End Function
     <Extension>
     Function IsBoolean(txt As String) As Boolean
+        If txt = Nothing Then
+            Return False
+        End If
         Return Boolean.TryParse(txt, Nothing)
     End Function
     <Extension>
     Function IsFraction(txt As String) As Boolean
+        If txt = Nothing Then
+            Return False
+        End If
         If txt.StartsWith("-") OrElse txt.StartsWith("+") Then txt = txt.Substring(1)
         If txt.CountOf("."c) <> 1 Then Return False
         Return txt.Length = Aggregate c In From t In txt.ToCharArray Take While t >= "0"c AndAlso t <= "9"c OrElse t = "." Into Count
